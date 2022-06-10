d8adfac72c3ef7a5c83544a1fe8f5b4028eb7dd5
Yevhenii Lapshynov <fd-freshjs>
2022-06-10 15:56:45 +0300

    Add alter table examples

--- ddl.sql
+++ ddl.sql
@@ -12,7 +12,7 @@ CREATE TABLE products (
 );
 
 -- @block
-INSERT INTO products (name) VALUES ('apple');
+INSERT INTO products (name, price) VALUES ('apple', -3.00);
 
 -- @block
 ALTER TABLE products
@@ -20,3 +20,37 @@ DROP CONSTRAINT "products_amount_check";
 
 ALTER TABLE products
 ADD CONSTRAINT "products_amount_check" CHECK (amount >= 0);
+
+-- @block
+ALTER TABLE products
+ADD COLUMN price numeric(7, 2);
+
+-- @block
+ALTER TABLE products
+DROP COLUMN price;
+
+
+
+
+
+-- @block
+ALTER TABLE products
+
+DROP COLUMN price,
+
+ADD COLUMN price numeric(7, 2) CHECK(price >= 0.00),
+
+ADD CONSTRAINT "max-value" CHECK(amount < 5000);
+
+
+
+
+-- @block
+ALTER TABLE products
+ALTER COLUMN price TYPE decimal(7, 2);
+
+ALTER TABLE products
+
+ADD CHECK (price >= 0.00),
+
+ALTER COLUMN price SET DEFAULT 0.00;
