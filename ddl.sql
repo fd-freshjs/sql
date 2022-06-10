e07c6868e038ae2852d49deaf5f397bd6721289c
Yevhenii Lapshynov <fd-freshjs>
2022-06-10 16:38:34 +0300

    Add primary key example

--- ddl.sql
+++ ddl.sql
@@ -3,7 +3,7 @@ DROP TABLE products;
 
 -- @block
 CREATE TABLE products (
-    id serial NOT NULL UNIQUE,
+    id serial PRIMARY KEY,
 
     amount int NOT NULL
     CHECK (amount > 0) DEFAULT 0,
@@ -27,9 +27,9 @@ ADD CHECK (amount >= 0);
 
 
 CREATE TABLE vet_client (
-    id serial NOT NULL UNIQUE,
+    id serial PRIMARY KEY,
     nickname VARCHAR(32) NOT NULL CHECK(nickname != ''),
-    phone VARCHAR(32) NOT NULL CHECK(nickname != ''),
+    phone VARCHAR(32) NOT NULL CHECK(phone != ''),
     full_name varchar(64),
     UNIQUE(nickname, phone)
 );
