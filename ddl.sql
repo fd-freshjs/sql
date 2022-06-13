7a363b0a7fc93fdafae751c37d886c67a771a617
Yevhenii Lapshynov <fd-freshjs>
2022-06-13 16:42:44 +0300

    Add many to many relation

--- ddl.sql
+++ ddl.sql
@@ -1,44 +1,67 @@
-/* 1-N один ко многим */
-
--- @block
-CREATE TABLE users (
-    -- id serial unique NOT NULL, -- users_id_key
-    id serial PRIMARY KEY, -- users_pkey
-    name varchar(32)
-);
-
-/* 
-    1-N
-
-    one user -> many phones
- */
-
-
-/* 1-0..1 */
-
-CREATE TABLE trainers (
-    id VARCHAR PRIMARY KEY,
-    full_name VARCHAR(64) NOT NULL CHECK(full_name != ''),
-    team_id int UNIQUE
-);
-
-CREATE TABLE teams (
-    id serial PRIMARY KEY,
-    name VARCHAR(32) NOT NULL UNIQUE CHECK(char_length(name) >= 4),
-    trainer_id VARCHAR REFERENCES trainers(id)
-);
-
-INSERT INTO trainers
-(id, full_name)
-VALUES
-('123-234-345-456', 'Test Testovich');
-
-INSERT INTO teams (name, trainer_id) VALUES ('Byki', '234-234-345-345'), ();
-
-UPDATE trainers
-SET team_id = 1
-WHERE -- filter
-"id" = '123-234-345-456';
-
-
 /* N-M */
+
+CREATE TABLE goods (
+    id serial PRIMARY KEY,
+    name varchar NOT NULL,
+    stock_amount int
+);
+
+CREATE TABLE orders (
+    id serial PRIMARY KEY,
+    address varchar
+);
+
+CREATE TABLE goods_to_orders (
+    order_id int REFERENCES orders(id),  -- 1, 1
+    good_id int REFERENCES goods(id), -- 1, 2
+    amount int NOT NULL CHECK(amount >= 1)
+);
+
+ALTER TABLE goods_to_orders
+ADD UNIQUE(order_id, good_id);
+
+/*  */
+
+INSERT INTO goods
+(name, stock_amount)
+VALUES (
+    'stool', 5
+), (
+    'table', 10
+), (
+    'candelabre', 15
+);
+
+INSERT INTO orders
+(address)
+VALUES (
+    'Zaporizha'
+),  (
+    'Kiev'
+), (
+    'Ney York'
+);
+
+INSERt INTO goods_to_orders
+VALUES (
+    1, 2, 5
+);
+
+INSERt INTO goods_to_orders
+VALUES (
+    1, 2, 5
+);
+
+INSERt INTO goods_to_orders
+VALUES (
+    3, 1, 3
+);
+
+
+/*  */
+
+DELETE FROM goods_to_orders
+WHERE
+    order_id = 1
+    AND good_id = 2
+    AND amount = 5;
