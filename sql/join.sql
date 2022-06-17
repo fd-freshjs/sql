57a1c3d02cee2d6c7ec2559fb9818e118e40e20a
Yevhenii <freshjs.fte.@gmail.com>
2022-06-17 17:12:39 +0300

    Add multiple join example

--- sql/join.sql
+++ sql/join.sql
@@ -3,7 +3,9 @@
     users 1
         ||
     orders N
-        ||  p_to_o
+        ||
+        p_to_o
+        ||
     phones M
  */
 /*  */
@@ -50,11 +52,69 @@ SELECT * FROM
 WHERE count > avg("count");
 
 /* 
-  users.id   <=   orders.user_id
+  INNER JOIN table ON table.id  =  orders.user_id
  */
 
 /* 
+  Все заказы с определенной моделью (id) телефона
+
+  Кол-во телефонов в определенном заказе
+
+  Все модели телефонов конкретного заказа
+
   Извлечь модель самого популярного телефона
   p_to_o.amount
+  шаг 1 найти модель тел конкр заказа
+
+
  */
 
+SELECT o.*
+FROM orders as o
+  JOIN phones_to_orders as pto ON pto.order_id = o.id
+WHERE pto.phone_id = 3;
+/*  */
+SELECT pto.order_id
+FROM phones_to_orders as pto
+  JOIN phones as p ON pto.phone_id = p.id
+WHERE p.id = 3;
+/*  */
+
+SELECT sum(amount)
+FROM phones_to_orders
+WHERE order_id = 18;
+
+SELECT sum(pto.amount)
+FROM phones_to_orders as pto
+  JOIN orders as o ON order_id = id
+WHERE o.id = 18;
+
+/*  */
+
+/* 
+    Найти всех пользователей которые не делали заказы
+ */
+SELECT o.id as order_id, u.* FROM users as u
+  LEFT JOIN orders as o ON o.user_id = u.id
+WHERE o.id IS NULL;
+
+/*  */
+SELECT id from orders WHERE user_id = 59;
+-- DELETE FROM orders WHERE id = 37 OR id = 43 OR id
+DELETE FROM orders WHERE id IN (37, 43, 44, 45);
+DELETE FROM phones_to_orders WHERE order_id IN (37, 43, 44, 45);
+
+/* 
+
+  Найти указанные модели определенного юзера
+ */
+
+ --users.id phones.model
+SELECT
+  p.model, u.id
+FROM phones as p
+  JOIN phones_to_orders as pto ON pto.phone_id = p.id
+  JOIN orders as o ON pto.order_id = o.id
+  JOIN users as u ON o.user_id = u.id
+  
+WHERE u.id = 58 AND p.model IN ('3310', 'Honor1', 'A51');
